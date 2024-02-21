3c79e988d5539bfbddbf932835dda134847219b8
Sourabh-Mallapur <sourabhsmallapur@gmail.com>
2024-02-21 13:00:52 +0530

    Update LFSR.py

--- LFSR/LFSR.py
+++ LFSR/LFSR.py
@@ -1,20 +1,26 @@
 # Code to implement a Linear Feedback Shift Register
 
-seed = 10001
-cell = 5
-coeff = [1,5]
+seed = int(input("Initial state of the register (in binary): "))
+cell = len(str(seed))
+n = int(input("Enter no of coeff: "))
+coeff = []
+print("Bits to XOR (start at 1 from the left)")
+for i in range(n): 
+    coeff.append(int(input()))
 
 def lfsr(seed, cell, coeff):
+    bits = ''
     lfsrList = [0] + [int(i) for i in str(seed)] + [0]
-    iterations, temp = 1, 0
-    # print('0', lfsrList)
+    iterations = 1
     while(iterations < (2 ** cell)):
         lfsrList = [0] + lfsrList[:-1]
-        for i in coeff:
+        temp = lfsrList[coeff[0]]
+        for i in coeff[1:]:
             temp = temp ^ lfsrList[i]
         lfsrList[0] = temp
-        # print(iterations, lfsrList)
-        print(lfsrList[-1], end='')
+        bits += str(lfsrList[-1])
         iterations += 1
-        
-lfsr(seed, cell, coeff)
+    return bits
+
+bits = lfsr(seed, cell, coeff)
+print(bits)
