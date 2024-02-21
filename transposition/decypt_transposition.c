33e71c408f61b4eb85c9430b9152e182319ec16b
Sourabh-Mallapur <sourabhsmallapur@gmail.com>
2024-02-21 21:39:32 +0530

    Update decypt_transposition.c

--- transposition/decypt_transposition.c
+++ transposition/decypt_transposition.c
@@ -5,28 +5,51 @@
 void decrypt_transposition(char* ciphertext, int key, int blocksize, char* decrypted_plaintext) {
     int dekey = 0;
     int temp = 1;
-    int temp2 = 0;
+    int digit, divisor_copy;
+    int divisor = 1;
+    int temp_key = key;
 
     for(int i = 1; i < blocksize; i++)
-        temp *= 10;
+        divisor *= 10;
     
-    for(int i = 0; i < blocksize; i++){
-        temp2 += ((i + 1) * (temp));
-        temp /= 10; }
-    
-    
-    char decryptmatrix[rows][cols];
+    divisor_copy = divisor;
 
-    // Decrypt using the key
-    index = 0;
-    for (int i = 0; i < cols; i++) {
-        int col = dekey % 10;
-        for (int j = 0; j < rows; j++) {
-            decrypted_plaintext[index++] = matrix[j][col - 1];
-        }
-        key /= 10;
+    for(int i = 1; i < blocksize + 1; i++) {
+        digit = key / divisor;
+        for(int j = digit; j < blocksize; j++)
+            temp *= 10;
+        dekey += (i * temp);
+        temp = 1;
+        key %= divisor;
+        divisor /= 10;
     }
-    decrypted_plaintext[length] = '\0'; // Null-terminate the string
+
+    // printf("%d", dekey);
+
+    int size = strlen(ciphertext);
+    
+    char ciphermatrix [size/blocksize][blocksize];
+    for (int i = 0; i < blocksize; i++) {
+        for (int j = 0; j < size/blocksize; j++) {
+            ciphermatrix[i][j] = ciphertext[(i * (size/blocksize)) + j];
+        } 
+        ciphermatrix[i][blocksize - 1] = '\0';
+    }
+
+    int index = 0;
+    divisor = divisor_copy;
+    key = temp_key;
+    while (divisor > 0) {
+        int row = key / divisor - 1; // Adjust key to 0-based index
+        for (int i = 0; i < size / blocksize; i++) {
+            decrypted_plaintext[index++] = ciphermatrix[row][i];
+            // printf("%c",ciphermatrix[row][i] );
+        }
+        key %= divisor;
+        divisor /= 10;
+    }
+    decrypted_plaintext[size] = '\0';
+
 }
 
 int main() {
